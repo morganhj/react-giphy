73619a47f6eb503b11cbdb9d0907dceeb659bdf5
morgan hoare <hoaremorgan@gmail.com>
2020-03-15 20:33:13 -0300

    added boilerplate

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Component } from 'react';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { clicked: false }; // defines initial state
+  }
+
+  handleClick = () => {
+    this.setState({ clicked: true });
+  }
+
+  render() {
+    return (
+      <h1 className={this.state.clicked ? "clicked" : ""}
+          onClick={this.handleClick}>
+        Hello,
+        {this.props.name}
+      </h1>
+    );
+  }
+}
+
+
+// const Hello = ({ name }) => {
+//   return (
+//     <div className="container">
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name=" Morgan" />, root);
 }
